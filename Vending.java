ffecd997def2c8975f568123309c28df0cf10221
Jaden <arceneauxjaden@gmail.com>
2019-12-18 02:31:23 +0000

    pretty much made the whole thing ;)

--- Vending.java
+++ Vending.java
@@ -3,10 +3,21 @@ import java.io.*;
 
 class Vending
 {
-  public String fileName;
+  private String fileName;
+  private double userMoney;
+  private double userSpending;
+  private ArrayList<Integer> accepedCash = new ArrayList<Integer>();
+
   public Vending(String userFileName)
   {
+    
     fileName = userFileName;
+    userMoney = 0.0;
+    userSpending = 0.0;
+    accepedCash.add(1);
+    accepedCash.add(5);
+    accepedCash.add(10);
+    accepedCash.add(20);
   }
 
   private ArrayList<String> readFile()
@@ -54,6 +65,18 @@ class Vending
     }
   }
 
+  private  void showItems(ArrayList<String> items, ArrayList<Double> prices, double max)
+  {
+    for (String item: items )
+    {
+      int index = items.indexOf(item);
+      if (prices.get(index) <= max)
+      {
+        System.out.println(items.get(index) + " " + prices.get(index));
+      }
+    }
+  }
+
   private boolean confirmPrices()
   {
     System.out.println("would you like to go into user mode(yes or no)");
@@ -61,23 +84,70 @@ class Vending
     return(userIn.nextLine().equals("yes"));
   }
 
-  private void selectItem(ArrayList<String> items, ArrayList<Double> prices)
+  private void selectItem(ArrayList<String> items, ArrayList<Double> prices, double max)
   {
-    showItems(items, prices);
+    showItems(items, prices, max);
     System.out.println("Which one would you like");
     Scanner userIn = new Scanner(System.in);
     String responce = userIn.nextLine();
     try
     {
       int index = items.indexOf(responce);
-      System.out.println(items.get(index) + " " + prices.get(index));
+      userMoney -= prices.get(index);
+      System.out.println("you have $" + userMoney + " left");
+      System.out.println("would you like to purchase more items");
+      Scanner userIn = new Scanner(System.in);
+      String userChoice = userIn.nextLine();
+      if ()
     }
     catch(Exception e)
     {
       System.out.println("item " + e + "does not exist");
-      selectItem(items, prices);
+      selectItem(items, prices, max);
     }
   }
+
+  public double insertMoney()
+  {
+    System.out.println("please insert one of the following monetary denominations and say \"done\" when you are finished ");
+    System.out.println(accepedCash);
+
+    Scanner moneyScanner = new Scanner(System.in);
+    String money = moneyScanner.next();
+    try
+    {
+      userMoney += Double.valueOf(money);
+    }
+    catch(Exception e)
+    {
+      if (money.equals("done"))
+      {
+        
+      }
+      insertMoney();
+    }
+
+    while (!(money.equals("done")))
+    {
+      moneyScanner = new Scanner(System.in);
+      money = moneyScanner.next();
+      try
+      {
+        userMoney += Double.valueOf(money);
+        System.out.println("Total: " + userMoney);
+      }
+      catch(Exception e)
+      {
+        if (money.equals("done"))
+        {
+          break;
+        }
+        insertMoney();
+      }
+    } 
+    return userMoney;
+    //Scanner 
+  } 
 
 
   public void start()
@@ -89,7 +159,8 @@ class Vending
     {
       while(true != false)
       {
-        selectItem(items, prices);
+        userMoney = 0;
+        selectItem(items, prices, insertMoney());
       }
     }
     else
